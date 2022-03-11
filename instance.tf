abe82c301d712e9d981f4b7186b5991381bd97bb
shegoj <shegoj@yahoo.com>
2022-03-11 20:07:13 +0000

    changed node names as required

--- instance.tf
+++ instance.tf
@@ -2,6 +2,6 @@ module "spin_up_machine" {
     source =  "./mymodules"
     instance_count = 2
     modulescripts  = ["scripts/apache.sh","scripts/tomcat.sh"]
-    node_name = ["apache_Web_Server","Tomcat App Server"] 
+    node_name = ["Web Server","App Server"] 
     environment = "dev" 
 }
