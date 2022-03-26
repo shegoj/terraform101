7fa5cd2e9c143bd7acc1ab54eeae14e24d7f4188
shegoj <shegoj@yahoo.com>
2022-03-26 20:23:50 +0000

    updated code

--- instance.tf
+++ instance.tf
@@ -2,6 +2,6 @@ module "spin_up_machine" {
     source =  "./mymodules"
     instance_count = 2
     modulescripts  = ["scripts/apache.sh","scripts/tomcat.sh"]
-    node_name = ["Web","App"] 
+    node_name = ["Webstuff","Appstuff"] 
     environment = "dev" 
 }
